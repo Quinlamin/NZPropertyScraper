e2b42af13dc9711b0d942c3f05496b79ccd6f013
GigantiRaptor Claasen <claasenclan@gmail.com>
2026-09-07 12:19:02 +1200

    WaipaDCScrape now functional and in headless mode, outputs a property class with desired information including: Capital Value, Land Value, and Total Rates.

--- NZPropertyScraper/WaipaDCScrape.cs
+++ NZPropertyScraper/WaipaDCScrape.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -9,17 +10,39 @@ namespace NZPropertyScraper
 {
     public static class WaipaDCScrape
     {
-        public static string driverLocation = string.Empty;
+        public static string driverLocation = "DRIVERS/geckodriver.exe";
 
-        public static void Initialize(string path)
+        
+        public class Property
         {
-            driverLocation = path;
-
+            public int capVal;
+            public int landVal;
+            public float rates;
+            public Dictionary<string, string> propertyValues;
+            public Property(Dictionary<string,string> _propertyValues)
+            {
+                foreach (var keyVal in _propertyValues) { 
+                    if(keyVal.Key.Contains("Capital Value"))
+                    {
+                        capVal = int.Parse(keyVal.Value.Replace("$", ""));
+                    }else if (keyVal.Key.Contains("Land Value"))
+                    {
+                        landVal = int.Parse(keyVal.Value.Replace("$", ""));
+                    }
+                    else if (keyVal.Key.Contains("Total Rates"))
+                    {
+                        rates = float.Parse(keyVal.Value.Replace("$", ""));
+                    }
+                }
+                propertyValues = _propertyValues;
+            }
         }
-
-        public static void PropertyAndRatesWaipa()
+        public static Property PropertyAndRatesWaipa(string inputValue)
         {
-            using (var driver = new FirefoxDriver(driverLocation))
+            Dictionary<string, string> propertyValues = new Dictionary<string, string>();
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArgument("--headless");
+            using (var driver = new FirefoxDriver(driverLocation,options))
             {
                 driver.Navigate().GoToUrl("https://waipadc.spatial.t1cloud.com/spatial/IntraMaps/ApplicationEngine/frontend/mapbuilder/default.htm?configId=6aa41407-1db8-44e1-8487-0b9a08965283&liteConfigId=9814f62a-448c-4a33-b101-4cf6cac0995a&title=UmF0ZXMlMjBJbmZvcm1hdGlvbg==");
                 IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
@@ -39,9 +62,57 @@ namespace NZPropertyScraper
                     }
                 }
                 Actions actions = new Actions(driver);
-                inputField.Click();
-                Console.ReadLine();
+                
+                actions.MoveToElement(inputField).Click().Build().Perform();
+                inputField.SendKeys(inputValue);
+
+               
+                var otherButton = driver.FindElement(By.CssSelector("button.mb-landscape-home"));
+                actions.MoveToElement(otherButton).Click().Build().Perform();
+                actions.MoveToElement(inputField).Click().Build().Perform();
+
+                IWebElement result;
+                while (true)
+                {
+                    try
+                    {
+                        result = driver.FindElement(By.CssSelector("li"));
+                        break;
+                    }
+                    catch (Exception)
+                    {
+
+                        
+                    }
+                }
+                result.Click();
+
+                while (true)
+                {
+                    try
+                    {
+                        driver.FindElement(By.CssSelector("label.mb-fields-row__label"));
+                        driver.FindElement(By.CssSelector(".mb-fields-row__value"));
+                        break;
+                    }
+                    catch (Exception)
+                    {
+
+
+                    }
+                }
+
+                ReadOnlyCollection<IWebElement> fieldList = driver.FindElements(By.CssSelector("label.mb-fields-row__label"));
+                ReadOnlyCollection<IWebElement> valueList = driver.FindElements(By.CssSelector(".mb-fields-row__value"));
+                for (int i = 0; i < fieldList.Count; i++)
+                {
+                    //Console.WriteLine(fieldList[i].Text + " " + valueList[i].Text);
+                    propertyValues.Add(fieldList[i].Text, valueList[i].Text);
+                }
+                
+                
             }
+            return new Property(propertyValues);
         }
     }
 }
