849151e1c5a71129baae901c44a80d3016cd8865
GigantiRaptor Claasen <claasenclan@gmail.com>
2026-09-14 13:57:35 +1200

    removed RealEstate Scrape due to compliance

--- NZPropertyScraper/WaipaDCScrape.cs
+++ NZPropertyScraper/WaipaDCScrape.cs
@@ -61,7 +61,7 @@ namespace NZPropertyScraper
             
             options.AddArgument("--headless");
             
-            using (var driver = new FirefoxDriver(driverLocation,options))
+            using (var driver = new FirefoxDriver(options))
             {
                 
                 driver.Navigate().GoToUrl("https://waipadc.spatial.t1cloud.com/spatial/IntraMaps/ApplicationEngine/frontend/mapbuilder/default.htm?configId=6aa41407-1db8-44e1-8487-0b9a08965283&liteConfigId=9814f62a-448c-4a33-b101-4cf6cac0995a&title=UmF0ZXMlMjBJbmZvcm1hdGlvbg==");
